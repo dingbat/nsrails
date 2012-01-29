386dc0807f723c661a5b0f938062c6e7ea1ff412
Dan Hassin <dahassin@mac.com>
2012-01-29 17:46:55 -0500

    added test examples to do later...

--- tests/Tests/MyTest.m
+++ tests/Tests/MyTest.m
@@ -19,4 +19,20 @@
 	GHAssertTrue(1+1==2, @"yep");
 }
 
+- (void)setUpClass {
+	// Run at start of all tests in the class
+}
+
+- (void)tearDownClass {
+	// Run at end of all tests in the class
+}
+
+- (void)setUp {
+	// Run before each test method
+}
+
+- (void)tearDown {
+	// Run after each test method
+} 
+
 @end
