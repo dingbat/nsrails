a1111433ac5433b7b12740e3b49c1de24f4100d7
Dan Hassin <danhassin@mac.com>
2012-02-22 01:37:07 -0500

    Added profanity filter to webapp

--- demo/nsrails.com/app/models/post.rb
+++ demo/nsrails.com/app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ActiveRecord::Base
   validates_presence_of :author
   
   accepts_nested_attributes_for :responses
+  
+  validate :deny_profanity
+  
+  private
+  def deny_profanity
+    if ProfanityFilter::Base.profane?(body)
+      errors.add :body, 'profanity'
+    end
+    if ProfanityFilter::Base.profane?(author)
+      errors.add :author, 'profanity'
+    end
+  end
 end
