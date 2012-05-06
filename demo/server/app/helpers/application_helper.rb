dd09ed50601ed6b839bd6c5a32dbf8715e9a4e92
Dan Hassin <danhassin@mac.com>
2012-05-06 19:08:03 -0400

    NSRails.com facelift

--- demo/server/app/helpers/application_helper.rb
+++ demo/server/app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     link_to content, "http://vimeo.com/dq/nsrails"
   end
   
-  def fork_me_ribbon(link)
+  def fork_me_ribbon_link(link)
     image = image_tag "http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png", :id=>"ribbon", :alt=>"Fork me on GitHub"
     link_to image, link
   end
