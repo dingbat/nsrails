5b0d3bb729aa47605c809c1659e9d8a6126cf963
Scott Klein <scott@scottkle.in>
2012-06-06 20:47:42 -0400

    if camelized property ends in Id, change it to ID

--- nsrails/Source/Categories/NSString+Inflection.m
+++ nsrails/Source/Categories/NSString+Inflection.m
@@ -129,6 +129,10 @@
 			}
 		}
 	}
+
+    // replace items that end in Id with ID
+    if([[underscored substringWithRange:NSMakeRange(underscored.length - 2, 2)] isEqualToString:@"Id"])
+        [underscored replaceCharactersInRange:NSMakeRange(underscored.length - 2, 2) withString:@"ID"];
 	
 	return underscored;
 }
